c1eb183c96e2540abc8223767158b66c106630c7
wags444 <bwags4@gmail.com>
2026-06-01 12:42:14 -0700

    Install exact packages needed by runtime_daily.R

--- install_packages.R
+++ install_packages.R
@@ -1,1 +1,1 @@
-install.packages(c("httr","jsonlite","dplyr","lubridate","glmnet","stringr","tidyr","Matrix"), repos="https://cloud.r-project.org", dependencies=TRUE)
+install.packages(c("httr","jsonlite","dplyr","purrr","lubridate","zoo","glmnet","Matrix","stringr","tidyr"), repos="https://cloud.r-project.org", dependencies=TRUE)
